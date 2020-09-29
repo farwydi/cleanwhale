f54dc60a072f67216b87614b79731e10e7ba2fe0
Leonid Zharikov <leon.zharikov@gmail.com>
2020-09-30 00:02:50 +0300

    feat: Add metrics package

--- go.mod
+++ go.mod
@@ -7,11 +7,11 @@ require (
 	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2
 	github.com/jinzhu/configor v1.2.0
 	github.com/pkg/errors v0.8.1
+	github.com/prometheus/client_golang v1.7.1
 	github.com/stretchr/testify v1.5.1
 	go.uber.org/zap v1.16.0
-	golang.org/x/sync v0.0.0-20190423024810-112230192c58
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e
 	google.golang.org/grpc v1.32.0
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 	gorm.io/driver/postgres v1.0.2
 	gorm.io/gorm v1.20.2
---
+++ metrics/register.go
@@ -0,0 +1,80 @@
+// Package metrics helper functions for prometheus and basic metrics.
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"go.uber.org/zap"
+)
+
+var (
+	// RequestCount counts the total number of requests.
+	RequestCount *prometheus.CounterVec
+
+	// RequestDuration counts the execution time of requests.
+	RequestDuration *prometheus.SummaryVec
+
+	// ResponseSize counts the number of bytes sent.
+	ResponseSize *prometheus.SummaryVec
+
+	// RequestSize counts the number of bytes received.
+	RequestSize *prometheus.SummaryVec
+)
+
+// RegisterMetrics base http metrics in prometheus.
+func RegisterMetrics(subsystem string, logger *zap.Logger) {
+	RequestCount = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Subsystem: subsystem,
+			Name:      "requests_total",
+			Help:      "How many HTTP requests processed, partitioned by status code and HTTP method.",
+		},
+		[]string{"code", "version", "method", "host", "handler"},
+	)
+	if err := prometheus.Register(RequestCount); err != nil {
+		logger.Fatal("fail register metrics",
+			zap.String("metric", "requests_total"),
+			zap.Error(err))
+	}
+
+	RequestDuration = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Subsystem: subsystem,
+			Name:      "request_duration_seconds",
+			Help:      "The HTTP request latencies in seconds.",
+		},
+		[]string{"code", "version", "method", "host", "handler"},
+	)
+	if err := prometheus.Register(RequestDuration); err != nil {
+		logger.Fatal("fail register metrics",
+			zap.String("metric", "request_duration_seconds"),
+			zap.Error(err))
+	}
+
+	ResponseSize = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Subsystem: subsystem,
+			Name:      "response_size_bytes",
+			Help:      "The HTTP response sizes in bytes.",
+		},
+		[]string{"code", "version", "method", "host", "handler"},
+	)
+	if err := prometheus.Register(ResponseSize); err != nil {
+		logger.Fatal("fail register metrics",
+			zap.String("metric", "response_size_bytes"),
+			zap.Error(err))
+	}
+
+	RequestSize = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Subsystem: subsystem,
+			Name:      "request_size_bytes",
+			Help:      "The HTTP request sizes in bytes.",
+		},
+		[]string{"code", "version", "method", "host", "handler"},
+	)
+	if err := prometheus.Register(RequestSize); err != nil {
+		logger.Fatal("fail register metrics",
+			zap.String("metric", "request_size_bytes"),
+			zap.Error(err))
+	}
+}
